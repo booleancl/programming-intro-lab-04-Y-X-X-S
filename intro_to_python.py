4e81a19964b54136ea3dd39ebb636bbbf1b79876
Yariyari <yaritza.urbina.sanhueza@gmail.com>
2022-09-02 11:05:41 -0400

    induccion python2

--- intro_to_python.py
+++ intro_to_python.py
@@ -3,7 +3,19 @@
 # Esta es la funcion para imprimir un mensaje en la terminal 
 print("Hola mundo")
 
-
-
+#la indentacion es importante. Es parte de la sintaxis
+#En este caso la linea 8 debe ir indentada para ser considerada
 if 5 > 2:
     print("Cinco es mayor que dos")
+
+#Cuando lleva comillas es texto y no se ejecuta la expresion. 
+#Cuando no lleva comillas si se ejecuta la expresion.
+    print(40+2)
+    print("40+2")
+
+#Operaiones matematicas.
+print(7*6)
+print(42/6)
+
+#Si no pongo comillas en una palabra, es sintaxis invalida
+print(Hola mundo)
